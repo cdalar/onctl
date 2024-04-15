2c38b8a9155bfd79903b5a1b842c2f3d1d6f800d
Cemal Y. Dalar <cemal@dalar.net>
2024-04-15 18:36:18 +0200

    download beta

---
+++ docs/docs/get_beta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Repository details
+REPO="cdalar/onctl"
+GITHUB="https://github.com"
+
+# Construct download URL
+# This URL pattern is an example and needs to match the actual pattern used in the releases
+download_url="$GITHUB/$REPO/releases/download/v0.1.0/onctl-linux"
+
+# Download the binary
+curl -L $download_url -o "onctl"
+
