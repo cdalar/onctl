d04faffc22eb80a8f151519b2c46a834487c7d4d
Cemal Y. Dalar <cemal@dalar.net>
2024-11-23 19:02:22 +0100

    refactor init

--- cmd/init.go
+++ cmd/init.go
@@ -6,33 +6,52 @@ import (
 	"os"
 
 	"github.com/cdalar/onctl/internal/files"
-
 	"github.com/spf13/cobra"
+)
+
+const (
+	onctlDir = ".onctl"
+	initDir  = "init"
 )
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "init onctl environment",
 	Run: func(cmd *cobra.Command, args []string) {
-		if _, err := os.Stat(".onctl"); os.IsNotExist(err) {
-			if err := os.Mkdir(".onctl", os.ModePerm); err != nil {
-				log.Fatal(err)
-			}
-			embedDir, err := files.EmbededFiles.ReadDir("init")
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, configFile := range embedDir {
-				log.Println("[DEBUG] initFile:" + configFile.Name())
-				eFile, _ := files.EmbededFiles.ReadFile("init/" + configFile.Name())
-				err = os.WriteFile(".onctl/"+configFile.Name(), eFile, 0644)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			fmt.Println("onctl environment initialized")
-		} else {
-			fmt.Println("onctl environment already initialized")
+		if err := initializeOnctlEnv(); err != nil {
+			log.Fatal(err)
 		}
 	},
+}
+
+func initializeOnctlEnv() error {
+	if _, err := os.Stat(onctlDir); os.IsNotExist(err) {
+		if err := os.Mkdir(onctlDir, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create %s directory: %w", onctlDir, err)
+		}
+		return populateOnctlEnv()
+	}
+	fmt.Println("onctl environment already initialized")
+	return nil
+}
+
+func populateOnctlEnv() error {
+	embedDir, err := files.EmbededFiles.ReadDir(initDir)
+	if err != nil {
+		return fmt.Errorf("failed to read embedded files: %w", err)
+	}
+
+	for _, configFile := range embedDir {
+		log.Println("[DEBUG] initFile:", configFile.Name())
+		eFile, err := files.EmbededFiles.ReadFile(initDir + "/" + configFile.Name())
+		if err != nil {
+			return fmt.Errorf("failed to read file %s: %w", configFile.Name(), err)
+		}
+
+		if err := os.WriteFile(onctlDir+"/"+configFile.Name(), eFile, 0644); err != nil {
+			return fmt.Errorf("failed to write file %s: %w", configFile.Name(), err)
+		}
+	}
+	fmt.Println("onctl environment initialized")
+	return nil
 }
