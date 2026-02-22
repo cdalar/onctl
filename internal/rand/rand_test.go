3718a64d7345799660017721e6ace718d2f45f39
Cemal Y. Dalar <cemal@dalar.net>
2026-02-22 08:57:20 +0300

    revert: remove test file changes, covered by codecov exclusion

--- internal/rand/rand_test.go
+++ internal/rand/rand_test.go
@@ -5,27 +5,6 @@ import (
 	"testing"
 )
 
-func TestPassword(t *testing.T) {
-	pwd := Password(12)
-	if len(pwd) != 12 {
-		t.Errorf("Password length should be 12, got %d", len(pwd))
-	}
-	// Password should start with an alphabetic character
-	const numericset = "0123456789"
-	if strings.Contains(numericset, string(pwd[0])) {
-		t.Errorf("Password should not start with numeric char, got: %s", pwd)
-	}
-}
-
-func TestPassword_Length(t *testing.T) {
-	for _, l := range []int{8, 16, 32} {
-		pwd := Password(l)
-		if len(pwd) != l {
-			t.Errorf("Password(%d) length = %d, want %d", l, len(pwd), l)
-		}
-	}
-}
-
 func Test_String(t *testing.T) {
 	seededRand.Seed(1)
 	str := String(10)
